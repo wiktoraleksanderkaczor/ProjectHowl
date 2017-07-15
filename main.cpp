c9087b926cab570b6048c4a894e89b06b09d792b
Wiktor Kaczor <wiktoraleksanderkaczor@gmail.com>
2017-07-15 21:50:38 +0100

    I changed the normal edit control to a rich text editor (added new method called CreateRichEdit). I also reordered the switch statement in WndProc for quicker break while resizing.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <iostream>
+#include <Commctrl.h>
 
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
@@ -11,6 +12,7 @@ HWND hWndEdit;
 //Declaring used methods.
 void getWindowSize();
 void AddMenus(HWND hWnd);
+HWND CreateRichEdit(HWND hWndOwner, int x, int y, int width, int height, HINSTANCE hInstance);
 
 //Defining params for menubar messages.
 #define IDM_FILE_NEW 1
@@ -19,11 +21,15 @@ void AddMenus(HWND hWnd);
 #define IDM_FILE_EXPORT 4
 #define IDM_HELP_ABOUT 5
 
-
 //Handling uncaught exceptions.
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg) {
+	case WM_SIZE:
+		getWindowSize();
+		//The magic numbers 16 and 39 are the adjustments for the window bars. First one is width while the second is height.
+		SetWindowPos(hWndEdit, NULL, 0, 0, ((rect.right - rect.left) - 16), ((rect.bottom - rect.top) - 39), NULL);
+		break;
 	case WM_CREATE:
 		AddMenus(hWnd);
 		break;
@@ -52,11 +58,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		PostQuitMessage(EXIT_SUCCESS);
 		break;
-	case WM_SIZE:
-		getWindowSize();
-		//The magic numbers 16 and 39 are the adjustments for the window bars.
-		SetWindowPos(hWndEdit, NULL, 0, 0, ((rect.right - rect.left) - 16), ((rect.bottom - rect.top) - 39), NULL);
-		break;
 	default:
 		return DefWindowProc(hWnd, msg, wParam, lParam);
 		break;
@@ -65,8 +66,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 }
 
 //The main entry point for the program.
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR nCmdLine, int nCmdShow) 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR nCmdLine, int nCmdShow) 
 {
 	LPTSTR windowClass = TEXT("ProjectHowlApp");
 	LPTSTR windowTitle = TEXT("ProjectHowl");
@@ -102,13 +102,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	}
 
 	getWindowSize();
+	
+	/* Exchanged for a rich text editor window.
 
-	//Creating inner editing window.
-	//Add "ES_AUTOHSCROLL" to styles for scrolling horizontally.
-	hWndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), TEXT("Start Writing Motherfucker..."),
+		//Creating inner editing window.
+		//Add "ES_AUTOHSCROLL" to styles for scrolling horizontally.
+		hWndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), TEXT("Start Writing Motherfucker..."),
 		WS_CHILD | WS_VISIBLE | ES_WANTRETURN | WS_VSCROLL | ES_LEFT | ES_MULTILINE,
 		0, 0, 0, 0, hWnd, NULL, NULL, NULL);
+	*/
 
+	//Creating rich text editor window.
+	hWndEdit = CreateRichEdit(hWnd, 0, 0, 0, 0, hInstance);
+
+	//Setting focus to window.
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
@@ -159,4 +166,20 @@ void AddMenus(HWND hWnd) {
 
 	//Setting hWnd to use the menubar.
 	SetMenu(hWnd, hMenubar);
+}
+
+HWND CreateRichEdit(HWND hWndOwner, int x, int y, int width, int height, HINSTANCE hInstance) {
+	//Loading dependencies.
+	LoadLibrary(TEXT("Msftedit.dll"));
+	LoadLibrary(TEXT("Riched32.dll"));
+	LoadLibrary(TEXT("Riched20.dll"));
+
+	//Creating edit control
+	//Alternatives to ES_CENTER are ES_LEFT and ES_RIGHT.
+	HWND hWndEdit = CreateWindowEx(0, TEXT("RICHEDIT"), TEXT("Start Writing Motherfucker..."),
+		ES_MULTILINE | WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP | WS_VSCROLL | ES_CENTER,
+		x, y, width, height,
+		hWndOwner, NULL, hInstance, NULL);
+
+	return hWndEdit;
 }
